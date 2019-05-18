dc2f6fa3e4c2b8bfb0a89076faeae5fb733769b0
sihan-tawsik <sihantawsik@gmail.com>
2019-05-18 20:21:53 +0600

    experimental path for linux

--- controller/firefox.py
+++ controller/firefox.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from sys import platform
 
 
@@ -30,7 +31,9 @@ def get_path():
 
     if platform == "linux" or platform == "linux2":
         # TODO implement for LINUX platform, just assign the root_path variable
-        root_path = ''
+        root_path = os.path.expanduser("~")
+        root_path = os.path.join(root_path, ".mozilla",
+                                 "firefox")
         possible_paths = __find_possible_paths__(root_path)
         return possible_paths
     elif platform == "darwin":
@@ -41,7 +44,7 @@ def get_path():
         possible_paths = __find_possible_paths__(root_path)
         return possible_paths
     elif platform == "win32":
-        # TODO implement for LINUX platform, just assign the root_path variable
+        # TODO implement for windows platform, just assign the root_path variable
         root_path = ''
         possible_paths = __find_possible_paths__(root_path)
         return possible_paths
